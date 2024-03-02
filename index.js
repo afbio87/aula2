9fa915326948f2ea06f4c977cf1721abad6c552f
Fabio Luiz Massambani <fabio.massambani@fatec.sp.gov.br>
2024-03-02 12:10:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Olá mundo')
+
+const a = 10
+let b = 20
+let nome = "Fabio"
+
+console.log(nome)
+
+const total = a + b
+console.log(total)
+
+console.log("Total: " + total)
+
+console.log('Total: ${total}')
+
+// + - * /
+
+/*a b*/ 
